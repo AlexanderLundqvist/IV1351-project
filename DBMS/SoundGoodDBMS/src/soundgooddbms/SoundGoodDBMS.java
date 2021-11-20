825d4586a460dd5b9ebf6a47b3235bde5e90bcbe
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-11-20 01:13:37 +0100

    Did something

--- DBMS/SoundGoodDBMS/src/soundgooddbms/SoundGoodDBMS.java
+++ DBMS/SoundGoodDBMS/src/soundgooddbms/SoundGoodDBMS.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package soundgooddbms;
 
-/**
- *
- * @author Zander
- */
+/*********************************** README ************************************
+*
+* Sound Good school Database Management System (DBMS)
+* @author Alexander Lundqvist and Max Dern
+* Created: 19-11-2021
+*
+* About this class:
+* This class acts as controller for the DMBS project. It controlls the flow of 
+* the program with user input and function calls to relevant classes.
+*
+* 
+*******************************************************************************/
+
 public class SoundGoodDBMS {
 
+    
+    
     /**
-     * @param args the command line arguments
+     * Main method that controlls the program flow.
+     * @param args takes no argument
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Startup
+            // Load database
+            // Prompt user for operations
+            
+        // Execute operations
+        
+        // Manipulate database
+        
+        // End program
+            // Save database
     }
     
 }
