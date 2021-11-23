d7d9c55b9ca0dfd048232fe3aef3951a823d9f86
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-11-24 00:36:19 +0100

    Added correct structure for later tasks

---
+++ DBMS/SoundGoodDBMS/src/DatabaseModel/DBMSModel.java
@@ -0,0 +1,26 @@
+package DatabaseModel;
+
+/*********************************** README ************************************
+*
+* Title placeholder
+* @author Alexander Lundqvist
+* Created: 24-11-2021
+*
+* About this class:
+*
+*
+* Based on:
+* <a href="">Link</a>
+*
+*******************************************************************************/
+
+public class DBMSModel {
+    
+    // Default constructor
+    public DBMSModel() {
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
