3a160b48f3fd1a439e6372205cbf39a464960f5c
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-11-24 14:05:45 +0100

    Create CreateDatabase.sql

---
+++ DBMS/SQL/CreateDatabase.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database for Soundgood Music School
+--
+
+-- Created by: Alexander Lundqvist & Max Dern
+-- Date: 23-11-2021
+
+--
+-- Pre-work on the database before loading
+--
+
+
+
+--
+-- Creating tables in the database
+--
+
+CREATE TABLE "person" (
+    "SSN" serial NOT NULL,
+    "first_name" VARCHAR(30),
+    "last_name" VARCHAR(30),
+    "person_number" VARCHAR(12) UNIQUE,
+    "age" INT,
+    "email" VARCHAR(50) UNIQUE,
+    "phone_number" VARCHAR(20) UNIQUE,
+    "street" VARCHAR(30),
+    "zip_code" VARCHAR(10),
+    "city" VARCHAR(20),
+    PRIMARY KEY ("SSN")
+);
+
+CREATE TABLE "student" (
+  "student_id" INT NOT NULL,
+  "SSN" INT NOT NULL,
+  FOREIGN KEY (SSN) REFERENCES person(SSN)
+);
+
+CREATE TABLE "instructor" (
+  "instructor_id" INT NOT NULL,
+  "SSN" INT NOT NULL,
+  FOREIGN KEY (SSN) REFERENCES person(SSN)
+);
