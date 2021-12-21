31cbf67e6d7cfd453348d3dee915a695182bb1e8
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-12-21 12:13:05 +0100

    b

---
+++ Task 2/InitializeDatabase.sql
@@ -0,0 +1,19 @@
+-- Soundgood music school
+INSERT INTO soundgood_music_school (id, available_spots, street, city, zip_code, contact_details_id)
+  VALUES (101010101, 35, 'Storgatan 27', 'Stockholm', 11455, 1);
+
+-- Instructors
+INSERT INTO instructor (id, available_spots, street, city, zip_code, contact_details_id)
+  VALUES (101010101, 35, "Storgatan 27", "Stockholm", 11455, 1);
+
+-- Admin staff
+INSERT INTO administrative_staff (id, available_spots, street, city, zip_code, contact_details_id)
+  VALUES (101010101, 35, "Storgatan 27", "Stockholm", 11455, 1);
+
+-- Students
+
+
+-- Parents
+
+
+-- Personal data
