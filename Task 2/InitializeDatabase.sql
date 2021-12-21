3cc7031519f135b72dceb3fc2c9a364c2caeb2db
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-12-21 14:59:49 +0100

    HEEEEY MACARENA

--- Task 2/InitializeDatabase.sql
+++ Task 2/InitializeDatabase.sql
@@ -1,19 +1,95 @@
+--------------------------------------------------------------------------------
+------------- Tables involving the human actors in the model -------------------
+--------------------------------------------------------------------------------
+
 -- Soundgood music school
-INSERT INTO soundgood_music_school (id, available_spots, street, city, zip_code, contact_details_id)
-  VALUES (101010101, 35, 'Storgatan 27', 'Stockholm', 11455, 1);
-
--- Instructors
-INSERT INTO instructor (id, available_spots, street, city, zip_code, contact_details_id)
-  VALUES (101010101, 35, "Storgatan 27", "Stockholm", 11455, 1);
-
--- Admin staff
-INSERT INTO administrative_staff (id, available_spots, street, city, zip_code, contact_details_id)
-  VALUES (101010101, 35, "Storgatan 27", "Stockholm", 11455, 1);
-
--- Students
-
-
--- Parents
-
+INSERT INTO soundgood_music_school (available_spots, street, city, zip_code)
+  VALUES
+    (35, 'Storgatan 27', 'Stockholm', 11455);
 
 -- Personal data
+INSERT INTO personaldata (personal_number, first_name, last_name, age, street, city, zip_code, phone_number, email_adress)
+  VALUES
+    (199312095888, 'Ina', 'Heffernon', 28, 'Scheelevägen 36', 'Södertälje', 10847, 0722095927, 'iheffernon0@gmail.com'),
+    (200911274021, 'Amelita', 'Bittlestone', 12, 'Roslagsvägen 52', 'Uppsala', 11487, 0729903390, 'abittlestone1@live.se'),
+    (200103140039, 'Ilko', 'Romushkin', 20, 'Upplandsgatan 42', 'Stockholm', 10990, 0759801000, 'iromushkin2@yahoo.com'),
+    (199902032436, 'Kaile', 'McConnel', 25, 'Gåsvägen 94', 'Uppsala', 11956, 0705276325, 'kmcconnel3@gmail.com'),
+    (200210280297, 'Lars', 'Casajuana', 19, 'Lottagatan 21', 'Solna', 11574, 0744782685, 'lcasajuana4@kth.se'),
+    (199611138821, 'Corella', 'Binge', 25, 'Klotvägen 25', 'Stockholm', 10375, 0711581646, 'cbinge5@facebook.com'),
+    (199704219987, 'Catarina', 'Conachy', 24, 'Nyvägen 86', 'Stockholm', 11936, 0755902262, 'cconachy6@livejournal.com'),
+    (199309160776, 'Filberto', 'Omand', 28, 'Lärkgången 30', 'Solna', 10596, 0720643974, 'fomand7@live.se'),
+    (200605237981, 'Doralynne', 'Sanderson', 15, 'Tusenleden 92', 'Stockholm', 10952, 0731276549, 'dsanderson8@hotmail.com'),
+    (199308190783, 'Briney', 'Amar', 28, 'Dackebacken 24', 'Södertälje', 10704, 0756296922, 'bamar9@gmail.com'),
+    (197304115156, 'Klas', 'Malmskog', 48, 'Valhallavägen 53', 'Stockholm', 10321, 0753516585, 'kmalm@gmail.com'), -- Teacher
+    (198406182581, 'Britta', 'Larsdotter', 37, 'Kullgatan 31', 'Södertälje', 10831, 0763723697, 'brittalars@gmail.com'), -- Teacher
+    (196909138375, 'Tage', 'Paulsson', 52, 'Järvaleden 216', 'Uppsala', 10584, 0736478357, 'tpaul@live.se'), -- Teacher
+    (196507052139, 'Ragnar', 'Landsberg', 44, 'Odengatan 75', 'Stockholm', 10237, 0756296922, 'rlandsberg@yahoo.com'), -- Admin
+    (195906137713, 'Konrad', 'Sanderson', 58, 'Odengatan 69', 'Stockholm', 10213, 0734525773, 'ksander@yahoo.com'), -- Admin
+    (197002083136, 'Loretta', 'Binge', 44, 'Odengatan 12', 'Stockholm', 10357, 0736456434, 'bingeDrink@yahoo.com'); -- Parent
+
+-- Instructors
+INSERT INTO instructor (employment_id, ensemble_teacher, instructor_expertise, personaldata_id, soundgood_music_school_id)
+  VALUES
+    ('AB123', 0, "Guitar, piano", (SELECT id from personaldata_id WHERE personal_number = 197304115156), 1),
+    ('RR420', 0, "Saxophone, bagpipe", (SELECT id from personaldata_id WHERE personal_number = 198406182581), 1),
+    ('LM666', 1, "Piano, guitar, flute, drums, fiddle", (SELECT id from personaldata_id WHERE personal_number = 196909138375), 1);
+
+-- Admin staff
+INSERT INTO administrative_staff (id, personaldata_id, soundgood_music_school_id)
+  VALUES
+    ((SELECT id from personaldata_id WHERE personal_number = 196507052139), 1),
+    ((SELECT id from personaldata_id WHERE personal_number = 195906137713), 1);
+
+-- Parents
+INSERT INTO parent (personaldata_id)
+  VALUES
+    ((SELECT id from personaldata_id WHERE personal_number = 197002083136));
+
+-- Students
+INSERT INTO student (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+
+--------------------------------------------------------------------------------
+------------------ Tables for the objects in the model -------------------------
+--------------------------------------------------------------------------------
+
+-- Instruments
+INSERT INTO parent (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+-- Pricing scheme
+INSERT INTO parent (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+-- Pricing scheme
+INSERT INTO parent (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+-- Pricing scheme
+INSERT INTO parent (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+-- Discounts
+INSERT INTO parent (id, instrument_quota, personaldata_id, parent_id)
+  VALUES
+    (1, 0);
+
+--------------------------------------------------------------------------------
+----------- Tables such as invoices, contracts or applications -----------------
+--------------------------------------------------------------------------------
+
+-- Instructor payments
+
+-- Student payments
+
+-- Lease contract_start_date
+
+-- Application forms
+
+-- Bookings
--- Task 2/SoundgoodLogicalModel.sql
+++ Task 2/SoundgoodLogicalModel.sql
@@ -1,14 +1,26 @@
-CREATE TABLE contact_details (
- id INT NOT NULL,
- phone_number VARCHAR(20) NOT NULL,
- email_adress VARCHAR(100) NOT NULL
-);
 
-ALTER TABLE contact_details ADD CONSTRAINT PK_contact_details PRIMARY KEY (id);
+DROP TABLE IF EXISTS "personaldata" CASCADE;
+DROP TABLE IF EXISTS "soundgood_music_school" CASCADE;
+DROP TABLE IF EXISTS "administrative_staff" CASCADE;
+DROP TABLE IF EXISTS "instructor" CASCADE;
+DROP TABLE IF EXISTS "instructor_salary" CASCADE;
+DROP TABLE IF EXISTS "parent" CASCADE;
+DROP TABLE IF EXISTS "pricing_scheme" CASCADE;
+DROP TABLE IF EXISTS "rental_instrument_inventory" CASCADE;
+DROP TABLE IF EXISTS "sibling_discount" CASCADE;
+DROP TABLE IF EXISTS "student" CASCADE;
+DROP TABLE IF EXISTS "student_invoice" CASCADE;
+DROP TABLE IF EXISTS "application_form" CASCADE;
+DROP TABLE IF EXISTS "bookings" CASCADE;
+DROP TABLE IF EXISTS "lease_contract" CASCADE;
+DROP TABLE IF EXISTS "music_lesson" CASCADE;
+DROP TABLE IF EXISTS "ensemble" CASCADE;
+DROP TABLE IF EXISTS "group_lesson" CASCADE;
+DROP TABLE IF EXISTS "individual_lesson" CASCADE;
 
 
 CREATE TABLE personaldata (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  personal_number VARCHAR(12) NOT NULL,
  first_name VARCHAR(1000),
  last_name VARCHAR(1000),
@@ -16,27 +28,26 @@ CREATE TABLE personaldata (
  street VARCHAR(1000),
  city VARCHAR(1000),
  zip_code VARCHAR(10),
- contact_details_id INT NOT NULL
+ phone_number VARCHAR(20) NOT NULL,
+ email_adress VARCHAR(100) NOT NULL
 );
 
 ALTER TABLE personaldata ADD CONSTRAINT PK_personaldata PRIMARY KEY (id);
 
 
 CREATE TABLE soundgood_music_school (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  available_spots INT,
  street VARCHAR(1000),
  city VARCHAR(1000),
- zip_code VARCHAR(10),
- contact_details_id INT NOT NULL
+ zip_code VARCHAR(10)
 );
 
 ALTER TABLE soundgood_music_school ADD CONSTRAINT PK_soundgood_music_school PRIMARY KEY (id);
 
 
 CREATE TABLE administrative_staff (
- id INT NOT NULL,
- admin_id VARCHAR(100) NOT NULL,
+ id SERIAL NOT NULL,
  personaldata_id INT NOT NULL,
  soundgood_music_school_id INT NOT NULL
 );
@@ -44,8 +55,20 @@ CREATE TABLE administrative_staff (
 ALTER TABLE administrative_staff ADD CONSTRAINT PK_administrative_staff PRIMARY KEY (id);
 
 
+CREATE TABLE bookings (
+ id SERIAL NOT NULL,
+ student_id VARCHAR(100),
+ lesson_date VARCHAR(10),
+ time_start VARCHAR(8),
+ time_end VARCHAR(8),
+ administrative_staff_id INT
+);
+
+ALTER TABLE bookings ADD CONSTRAINT PK_bookings PRIMARY KEY (id);
+
+
 CREATE TABLE instructor (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  employment_id VARCHAR(100) NOT NULL,
  ensemble_teacher BIT(1),
  instructor_expertise VARCHAR(500),
@@ -57,7 +80,7 @@ ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
 
 
 CREATE TABLE instructor_salary (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  amount_of_individual_lessons INT,
  amount_of_group_lessons INT,
  amount_of_ensemble INT,
@@ -69,8 +92,21 @@ CREATE TABLE instructor_salary (
 ALTER TABLE instructor_salary ADD CONSTRAINT PK_instructor_salary PRIMARY KEY (id);
 
 
+CREATE TABLE music_lesson (
+ id SERIAL NOT NULL,
+ room_number VARCHAR(10),
+ lesson_date VARCHAR(10),
+ time_start VARCHAR(8),
+ time_end VARCHAR(8),
+ bookings_id INT,
+ instructor_id INT
+);
+
+ALTER TABLE music_lesson ADD CONSTRAINT PK_music_lesson PRIMARY KEY (id);
+
+
 CREATE TABLE parent (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  personaldata_id INT NOT NULL
 );
 
@@ -90,7 +126,7 @@ ALTER TABLE pricing_scheme ADD CONSTRAINT PK_pricing_scheme PRIMARY KEY (soundgo
 
 
 CREATE TABLE rental_instrument_inventory (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  type_of_instrument VARCHAR(50),
  instrument_brand VARCHAR(500),
  number_in_stock INT,
@@ -109,7 +145,7 @@ ALTER TABLE sibling_discount ADD CONSTRAINT PK_sibling_discount PRIMARY KEY (sou
 
 
 CREATE TABLE student (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  instrument_quota INT,
  personaldata_id INT NOT NULL,
  parent_id INT NOT NULL
@@ -119,7 +155,7 @@ ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
 
 
 CREATE TABLE student_invoice (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  amount_of_individual_lessons INT,
  amount_of_group_lessons INT,
  amount_of_ensemble INT,
@@ -132,7 +168,7 @@ ALTER TABLE student_invoice ADD CONSTRAINT PK_student_invoice PRIMARY KEY (id);
 
 
 CREATE TABLE application_form (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  type_of_instrument VARCHAR(50),
  level_of_skill VARCHAR(20),
  soundgood_music_school_id INT NOT NULL,
@@ -140,41 +176,6 @@ CREATE TABLE application_form (
 );
 
 ALTER TABLE application_form ADD CONSTRAINT PK_application_form PRIMARY KEY (id);
-
-
-CREATE TABLE bookings (
- id INT NOT NULL,
- student_id VARCHAR(100),
- time_start TIMESTAMP(10),
- time_end TIMESTAMP(10),
- administrative_staff_id INT,
- instructor_id INT
-);
-
-ALTER TABLE bookings ADD CONSTRAINT PK_bookings PRIMARY KEY (id);
-
-
-CREATE TABLE lease_contract (
- id INT NOT NULL,
- type_of_instrument VARCHAR(50),
- contract_start_date DATE,
- contract_end_date DATE,
- student_id INT NOT NULL
-);
-
-ALTER TABLE lease_contract ADD CONSTRAINT PK_lease_contract PRIMARY KEY (id);
-
-
-CREATE TABLE music_lesson (
- id INT NOT NULL,
- room_number VARCHAR(10),
- time_start TIMESTAMP(10),
- time_end TIMESTAMP(10),
- bookings_id INT,
- instructor_id INT
-);
-
-ALTER TABLE music_lesson ADD CONSTRAINT PK_music_lesson PRIMARY KEY (id);
 
 
 CREATE TABLE ensemble (
@@ -199,21 +200,29 @@ ALTER TABLE group_lesson ADD CONSTRAINT PK_group_lesson PRIMARY KEY (music_lesso
 
 CREATE TABLE individual_lesson (
  music_lesson_id INT NOT NULL,
- student_id VARCHAR(100),
+ student_id INT,
  type_of_instrument VARCHAR(50)
 );
 
 ALTER TABLE individual_lesson ADD CONSTRAINT PK_individual_lesson PRIMARY KEY (music_lesson_id);
 
 
-ALTER TABLE personaldata ADD CONSTRAINT FK_personaldata_0 FOREIGN KEY (contact_details_id) REFERENCES contact_details (id);
+CREATE TABLE lease_contract (
+ id SERIAL NOT NULL,
+ type_of_instrument VARCHAR(50),
+ contract_start_date VARCHAR(10),
+ contract_end_date VARCHAR(10),
+ student_id INT NOT NULL
+);
 
-
-ALTER TABLE soundgood_music_school ADD CONSTRAINT FK_soundgood_music_school_0 FOREIGN KEY (contact_details_id) REFERENCES contact_details (id);
+ALTER TABLE lease_contract ADD CONSTRAINT PK_lease_contract PRIMARY KEY (id);
 
 
 ALTER TABLE administrative_staff ADD CONSTRAINT FK_administrative_staff_0 FOREIGN KEY (personaldata_id) REFERENCES personaldata (id);
 ALTER TABLE administrative_staff ADD CONSTRAINT FK_administrative_staff_1 FOREIGN KEY (soundgood_music_school_id) REFERENCES soundgood_music_school (id);
+
+
+ALTER TABLE bookings ADD CONSTRAINT FK_bookings_0 FOREIGN KEY (administrative_staff_id) REFERENCES administrative_staff (id);
 
 
 ALTER TABLE instructor ADD CONSTRAINT FK_instructor_0 FOREIGN KEY (personaldata_id) REFERENCES personaldata (id);
@@ -221,6 +230,10 @@ ALTER TABLE instructor ADD CONSTRAINT FK_instructor_1 FOREIGN KEY (soundgood_mus
 
 
 ALTER TABLE instructor_salary ADD CONSTRAINT FK_instructor_salary_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+
+
+ALTER TABLE music_lesson ADD CONSTRAINT FK_music_lesson_0 FOREIGN KEY (bookings_id) REFERENCES bookings (id);
+ALTER TABLE music_lesson ADD CONSTRAINT FK_music_lesson_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
 
 
 ALTER TABLE parent ADD CONSTRAINT FK_parent_0 FOREIGN KEY (personaldata_id) REFERENCES personaldata (id);
@@ -246,17 +259,6 @@ ALTER TABLE application_form ADD CONSTRAINT FK_application_form_0 FOREIGN KEY (s
 ALTER TABLE application_form ADD CONSTRAINT FK_application_form_1 FOREIGN KEY (student_id) REFERENCES student (id);
 
 
-ALTER TABLE bookings ADD CONSTRAINT FK_bookings_0 FOREIGN KEY (administrative_staff_id) REFERENCES administrative_staff (id);
-ALTER TABLE bookings ADD CONSTRAINT FK_bookings_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
-
-
-ALTER TABLE lease_contract ADD CONSTRAINT FK_lease_contract_0 FOREIGN KEY (student_id) REFERENCES student (id);
-
-
-ALTER TABLE music_lesson ADD CONSTRAINT FK_music_lesson_0 FOREIGN KEY (bookings_id) REFERENCES bookings (id);
-ALTER TABLE music_lesson ADD CONSTRAINT FK_music_lesson_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
-
-
 ALTER TABLE ensemble ADD CONSTRAINT FK_ensemble_0 FOREIGN KEY (music_lesson_id) REFERENCES music_lesson (id);
 
 
@@ -266,3 +268,4 @@ ALTER TABLE group_lesson ADD CONSTRAINT FK_group_lesson_0 FOREIGN KEY (music_les
 ALTER TABLE individual_lesson ADD CONSTRAINT FK_individual_lesson_0 FOREIGN KEY (music_lesson_id) REFERENCES music_lesson (id);
 
 
+ALTER TABLE lease_contract ADD CONSTRAINT FK_lease_contract_0 FOREIGN KEY (student_id) REFERENCES student (id);
