a615e3c2f19ca66da9b39f0f873a7bf9952dd608
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-04-01 14:41:08 +0200

    Update Main.java

--- DBMS/Main.java
+++ DBMS/Main.java
@@ -7,75 +7,84 @@ import java.sql.*;
  * @author Alexander Lundqvist & Max Dern
  */
 public class Main {
-    private PreparedStatement listAllInstrumentsStmt;
-    private PreparedStatement createNewRentalStmt;
-    private PreparedStatement terminateRentalStmt;
-    private PreparedStatement checkStudentRentalsStmt;
-    private Connection DBconnection;
+     private String port     = "5432";
+     private String database = "soundgood";
+     private String user     = "postgres";
+     private String password = "postgres";
 
-    private void connectToDatabase() throws SQLException, ClassNotFoundException {
-        try {
-            DBconnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/soundgood", "postgres", "postgres");
-            DBconnection.setAutoCommit(false);
-            System.out.println("Connection established!");
-        } catch (Exception ex) {
-            System.out.println("Couldn't connect to database...");
-        }
-    }
+     private PreparedStatement listAllInstrumentsStmt;
+     private PreparedStatement createNewRentalStmt;
+     private PreparedStatement terminateRentalStmt;
+     private PreparedStatement checkStudentRentalsStmt;
 
-    private void prepareStatements(Connection connection) throws SQLException {
-        listAllInstrumentsStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory WHERE rented = FALSE");
-        createNewRentalStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory");
-        terminateRentalStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory");
-    }
+     private Connection DBconnection;
 
-    private void interfaceMenu() {
-       System.out.println("*************************************************************");
-       System.out.println("********  Soundgood music school - Instrument rental ********");
-       System.out.println("*************************************************************");
-       System.out.println("*                                                           *");
-       System.out.println("* 1. List all instruments.                                  *");
-       System.out.println("* 2. Rent instrument.                                       *");
-       System.out.println("* 3. Terminate rental.                                      *");
-       System.out.println("* 4. Exit rental interface                                  *");
-       System.out.println("*                                                           *");
-       System.out.println("*************************************************************");
-    }
+     private Connection connectToDatabase() throws SQLException, ClassNotFoundException {
+         Connection DBconnection = null;
+         try {
+             Class.forName("org.postgresql.Driver");
+             DBconnection = DriverManager.getConnection("jdbc:postgresql://localhost:" + port + "/" + database, user, password);
+             DBconnection.setAutoCommit(false);
+             System.out.println("Connection established!");
+         } catch (Exception ex) {
+             System.out.println("Couldn't connect to database: " + ex);
+             ex.printStackTrace();
+         }
+         return DBconnection;
+     }
 
-    public static void main(String[] args) {
-        Main dbms = new Main();
-        try {
-            dbms.connectToDatabase();
-            Scanner scanner = new Scanner(System.in);
-            while(true) {
-                dbms.interfaceMenu();
-                System.out.println("Enter you choice: ");
-                int choice = scanner.nextInt();
-                System.out.println();
-                switch (choice) {
-                    case 1:
-                        System.out.println("Listing instruments");
-                        // Do something
-                        break;
-                    case 2:
-                        System.out.println("Enter rental process here");
-                        // Do something else
-                        break;
-                    case 3:
-                        System.out.println("Enter termination process here");
-                        // Terminate something
-                        break;
-                    case 4:
-                        System.out.println("\nExiting...");
-                        Thread.sleep(1200);
-                        System.exit(1);
-                        break;
-                }
-                System.out.println();
-            }
-        } catch (Exception ex) {
-            System.out.println("Error: " + ex);
-        }
+     private void prepareStatements(Connection connection) throws SQLException {
+         listAllInstrumentsStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory WHERE rented = FALSE");
+         createNewRentalStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory");
+         terminateRentalStmt = connection.prepareStatement("SELECT * FROM rental_instrument_inventory");
+     }
 
-    }
+     private void interfaceMenu() {
+        System.out.println("*************************************************************");
+        System.out.println("********  Soundgood music school - Instrument rental ********");
+        System.out.println("*************************************************************");
+        System.out.println("*                                                           *");
+        System.out.println("* 1. List all instruments.                                  *");
+        System.out.println("* 2. Rent instrument.                                       *");
+        System.out.println("* 3. Terminate rental.                                      *");
+        System.out.println("* 4. Exit rental interface                                  *");
+        System.out.println("*                                                           *");
+        System.out.println("*************************************************************");
+     }
+
+     public static void main(String[] args) {
+         Main dbms = new Main();
+         try {
+             Connection connection = dbms.connectToDatabase();
+             Scanner scanner = new Scanner(System.in);
+             while(true) {
+                 dbms.interfaceMenu();
+                 System.out.println("Enter you choice: ");
+                 int choice = scanner.nextInt();
+                 System.out.println();
+                 switch (choice) {
+                     case 1:
+                         System.out.println("Listing instruments");
+                         // Do something
+                         break;
+                     case 2:
+                         System.out.println("Enter rental process here");
+                         // Do something else
+                         break;
+                     case 3:
+                         System.out.println("Enter termination process here");
+                         // Terminate something
+                         break;
+                     case 4:
+                         System.out.println("\nExiting...");
+                         Thread.sleep(1200);
+                         System.exit(1);
+                         break;
+                 }
+                 System.out.println();
+             }
+         } catch (Exception ex) {
+             System.out.println("Error: " + ex);
+         }
+     }
 }
